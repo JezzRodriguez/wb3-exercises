53a60aae24a054cad3a31ba374d695cdae90974f
JezzRodriguez <jrodriguez@appdev.yearup.org>
2023-05-04 16:59:03 -0400

    Simple function workbook exercise Displaying things in the console

---
+++ functions/simple_functions.js
@@ -0,0 +1,9 @@
+'use strict'
+// Excercise 1 page 1-6
+function favThings() {
+    console.log('Jezz Rodriguez');
+    console.log("Favorite Movie: Love and basketball");
+    console.log("Favorite Musician: SZA");
+}
+favThings ();
+
