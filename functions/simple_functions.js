8ed9479af7ed7aa8c5dda727955694bd4fa11752
JezzRodriguez <jrodriguez@appdev.yearup.org>
2023-05-04 19:13:45 -0400

    finishing writing simple functions

--- functions/simple_functions.js
+++ functions/simple_functions.js
@@ -7,3 +7,12 @@ function favThings() {
 }
 favThings ();
 
+function whyImHere () {
+    console.log("I'm here because I want to get a good job in the developer field.");
+}
+whyImHere()
+
+function favPlace (){
+    console.log("My favorite place to visit is any place that could be considered a vacation because it's relaxing.");
+}
+favPlace()
